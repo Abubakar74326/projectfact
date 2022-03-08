c3e291e37ccb018b998f4efc2f401e81bc912e70
muhammad abubakar <mabubakarbssg@gmail.com>
2022-03-08 05:15:49 -0500

    API CODE using flask

--- api.py
+++ api.py
@@ -4,7 +4,6 @@
 from flask import Flask
 import factorial_file
 
-
 app = Flask(__name__)
 
 @app.get("/")
@@ -17,4 +16,4 @@ def read_item(num: int):
     return {"factorial": fact}
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8081, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
